676cef29e46f76c8e1289f26e76938be827bf296
Calico <github@hinrich.de>
2012-03-04 16:08:55 +0100

    Initial Commit

---
+++ FullscreenSlideshow/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  FullscreenSlideshow
+//
+//  Created by Hinrich Harms on 04.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ FullscreenSlideshow/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  FullscreenSlideshow
+//
+//  Created by Hinrich Harms on 04.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
